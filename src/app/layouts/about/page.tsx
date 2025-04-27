391975fca8399fff511c23359c02cf6b1d639cfe
Noble <basilgoodluck22@gmail.com>
2025-04-27 19:33:54 +0100

    resolved missing import

--- src/app/layouts/about/page.tsx
+++ src/app/layouts/about/page.tsx
@@ -5,7 +5,6 @@ export default function About(): JSX.Element {
   return (
     <section className="py-20 px-4 bg-white dark:bg-neutral-900">
       <Overview />
-      <Technologies />
     </section>
   );
 }
